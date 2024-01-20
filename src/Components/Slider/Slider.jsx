c0edcdc2700120e0f2bd217be7e7276012970405
615shoaib <615shoaib@gmail.com>
2024-01-20 02:24:29 -0800

    first commit

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,47 +1,68 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+const parseHTML = (htmlString) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, 'text/html');
+  return doc.body;
+};
 
-const Slider = () => {
-    const [data, setData] = useState([])
-
-    const getSlider = async () => {
-        try {
-            const slider = await axios.get(`http://localhost/wordpress/wp-json/wp/v2/pages`)
-            console.log(slider.data)
-            setData(slider.data)
-        } catch (error) {
-            console.log(`error message api failed `, error.message)
-        }
+const extractData = (body) => {
+    const containerElement = body.querySelector('.elementor-44');
+  
+    if (!containerElement) {
+      console.error('Container element not found');
+      return null;
     }
+  
+    const imageElement = containerElement.querySelector('.elementor-widget-image img');
+    const headingElement = containerElement.querySelector('.elementor-element-875eae7 h1');
+    const paragraphElement = containerElement.querySelector('.elementor-element-a6107fe .para p');
+  
+    const imageSrc = imageElement ? imageElement.getAttribute('src') : null;
+    const heading = headingElement ? headingElement.innerText : null;
+    const paragraph = paragraphElement ? paragraphElement.innerText : null;
+  
+    return { imageSrc, heading, paragraph };
+  };
+  
 
+  const Slider = () => {
+    const [data, setData] = useState(null);
+  
+    const getSlider = async () => {
+      try {
+        const response = await axios.get('http://localhost/wordpress/wp-json/wp/v2/pages/44');
+        const body = new DOMParser().parseFromString(response.data.content.rendered, 'text/html');
+        const extractedData = extractData(body);
+  
+        if (extractedData) {
+          setData(extractedData);
+        } else {
+          console.error('Failed to extract data');
+        }
+      } catch (error) {
+        console.error('API request failed:', error.message);
+      }
+    };
+  
     useEffect(() => {
-        getSlider()
-    }, [])
+      getSlider();
+    }, []);
+  
+    if (!data) {
+      return <p>Loading...</p>;
+    }
+  
+    // Render your data here using {data.imageSrc}, {data.heading}, {data.paragraph}
+  
     return (
-        <>
-            <div className='container'>
-                <div className='row'>
-                {
-                    Array.isArray(data) && data.map((slider) => (
-                        <>
-
-                            <div className='col-lg-12 d-flex justify-content-around'>
-                                
-                                <div dangerouslySetInnerHTML={{ __html: slider.excerpt.rendered }} />
-                                <p dangerouslySetInnerHTML={{ __html: slider.content.rendered }}/>
-                                
-                            </div>
-
-
-
-
-                        </>
-                    ))
-                }
-            </div>
-        </div>
-    </>
-  )
-}
-
-export default Slider
+      <div>
+        <img src={data.imageSrc} alt="Slider" style={{marginLeft:"90px"}}/>
+        <h1>{data.heading}</h1>
+        <p>{data.paragraph}</p>
+      </div>
+    );
+  };
+  
+  export default Slider;
+  
