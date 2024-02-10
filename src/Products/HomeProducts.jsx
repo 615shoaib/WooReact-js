b47fbbad17f9d2d3da2fa6d77fdcfc798d5a1e66
615shoaib <615shoaib@gmail.com>
2024-02-10 02:26:40 -0800

    first commit

--- src/Products/HomeProducts.jsx
+++ src/Products/HomeProducts.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const HomeProducts = ({selectedCategory}) => {
-   
-    console.log('no data ',selectedCategory)
-   
+const HomeProducts = ({ products }) => {
+    const filteredProducts = products.filter((product) => {
+        if (product.categories && product.categories.length > 0) {
+            return product.categories.some((category) => category.name === "NEW ARRIVALS");
+        }
+        return false;
+    });
 
-  
+    console.log(filteredProducts); 
 
     return (
         <>
-        <div className="d-flex text-center justify-content-center align-items-center flex-direction-column">
-      <h1 className='bg-info'>{selectedCategory}</h1>
-        </div>
+            <div className="d-flex text-center justify-content-center align-items-center flex-direction-column">
+                <h1 className='bg-info'>Featured Products</h1>
+                {filteredProducts.map((product) => (
+                    <div key={product.id}>
+                        <p>{product.name}</p>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
 
 export default HomeProducts;
-  
--- src/Products/WoocommerceApi.jsx
+++ src/Products/WoocommerceApi.jsx
@@ -1,19 +1,18 @@
-import {useContext} from "react"
-import { AppPrvoider } from '../Components/ContentApi/Api';
-import HomeProducts from './HomeProducts';
-import Category from './Category';
+import React, { useContext } from "react";
+import { AppPrvoider } from "../Components/ContentApi/Api";
+import HomeProducts from "./HomeProducts";
+import Category from "./Category";
 
 const WooCommerceExample = () => {
   const { products } = useContext(AppPrvoider);
- 
 
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-lg-12 pt-5 mt-5 ">
-          <Category products={products}/>
-      ds
+            <Category products={products} />
+            <HomeProducts products={products}/>
           </div>
         </div>
       </div>
