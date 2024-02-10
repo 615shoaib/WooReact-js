19e269190a419010354c4b0e3540d06ccc8b5873
615shoaib <615shoaib@gmail.com>
2024-02-10 04:01:21 -0800

    first commit

--- src/Products/FilterProducts/FilterProducts.jsx
+++ src/Products/FilterProducts/FilterProducts.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const FilterProducts = ({ products, setSortedProducts }) => {
 
     const handleSortChange = (event) => {
         const selectedOption = event.target.value;
         if (selectedOption === 'name') {
-            // Sort products by name (A-Z)
             const sortedData = [...products].sort((a, b) => a.name.localeCompare(b.name));
             setSortedProducts(sortedData);
         } else if (selectedOption === 'price') {
-            // Sort products by price (low to high)
             const sortedData = [...products].sort((a, b) => a.price - b.price);
             setSortedProducts(sortedData);
+        }else{
+          setSortedProducts(products)
         }
     };
 
     return (
-        <div className='text-center mb-5'>
-            <select className='mt-3 mb-3' onChange={handleSortChange}>
+      <div>
+        <Link to="/">&#11160; Back To Home</Link>
+        <div className='text-left mt-5 mb-5'>
+          <label htmlFor="">Sorted by Produts :   </label>
+            <select className='mb-3' onChange={handleSortChange}>
                 <option value="">--- Select ---</option>
                 <option value="name">Sort by Name</option>
                 <option value="price">Sort by Price</option>
             </select>
+        </div>
         </div>
     );
 }
--- src/Products/MoreProducts/MoreProducts.jsx
+++ src/Products/MoreProducts/MoreProducts.jsx
@@ -1,45 +1,50 @@
-  import React, { useContext, useState } from "react";
-  import { AppPrvoider } from "../../Components/ContentApi/Api";
-  import EyeIcone from "../EyeIcone";
-  import AddToCart from "../AddToCart";
-  import FilterProducts from "../FilterProducts/FilterProducts";
+import React, { useContext, useEffect, useState } from 'react';
+import { AppPrvoider } from '../../Components/ContentApi/Api';
+import EyeIcone from '../EyeIcone';
+import AddToCart from '../AddToCart';
+import FilterProducts from '../FilterProducts/FilterProducts';
 
-  const MoreProducts = () => {
+const MoreProducts = () => {
     const { products } = useContext(AppPrvoider);
+    const [sortedProducts, setSortedProducts] = useState([]);
 
-    const [sortedProducts, setSortedProducts] = useState(products);
-    console.log(sortedProducts)
+    useEffect(() => {
+        if (products && products.length > 0) {
+            setSortedProducts(products);
+        }
+    }, [products]);
+
     return (
-      <>
-        <div className="container">
-          <div className="row">
-            <FilterProducts products={products} setSortedProducts={setSortedProducts} />
-            {sortedProducts.map((detail) => (
-              <div className="col-lg-4 d-flex justify-content-center" key={detail.id}>
-                <div className="card border-0">
-                  <div className="card-img">
-                    {detail.images && detail.images.length > 0 && (
-                      <img
-                        src={detail.images[0].src}
-                        alt="products-card"
-                        style={{ width: "200px", height: "205px" }}
-                      />
-                    )}
-                    <div className="overlay">
-                      <EyeIcone card={detail} />
-                      <AddToCart />
-                    </div>
-                  </div> 
-                  <p className="text-center">{`Dress ${detail.name}`}</p>
-                  <p className="text-center" style={{ marginTop: "-15px" }} dangerouslySetInnerHTML={{ __html: detail.description }} />
-                  <p className="text-center" style={{ marginTop: "-25px" }}>${detail.price}.00</p>
+        <>
+            <div className="container">
+                <div className="row">
+                    <FilterProducts products={products} setSortedProducts={setSortedProducts} />
+                    {sortedProducts.map((detail) => (
+                        <div className="col-lg-4 d-flex justify-content-center" key={detail.id}>
+                            <div className="card border-0">
+                                <div className="card-img">
+                                    {detail.images && detail.images.length > 0 && (
+                                        <img
+                                            src={detail.images[0].src}
+                                            alt="products-card"
+                                            style={{ width: "200px", height: "205px" }}
+                                        />
+                                    )}
+                                    <div className="overlay">
+                                        <EyeIcone card={detail} />
+                                        <AddToCart />
+                                    </div>
+                                </div>
+                                <p className="text-center">{`Dress ${detail.name}`}</p>
+                                <p className="text-center" style={{ marginTop: "-15px" }} dangerouslySetInnerHTML={{ __html: detail.description }} />
+                                <p className="text-center" style={{ marginTop: "-25px" }}>${detail.price}.00</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </>
+            </div>
+        </>
     );
-  };
+};
 
-  export default MoreProducts;
+export default MoreProducts;
