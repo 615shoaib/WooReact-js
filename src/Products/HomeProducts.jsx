dfd62e9280f972ec2a1d88c8c0aa669586f98669
615shoaib <615shoaib@gmail.com>
2024-02-10 02:32:23 -0800

    first commit

--- src/Products/HomeProducts.jsx
+++ src/Products/HomeProducts.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import EyeIcone from './EyeIcone';
+import AddToCart from './AddToCart';
 
 const HomeProducts = ({ products }) => {
     const filteredProducts = products.filter((product) => {
         if (product.categories && product.categories.length > 0) {
-            return product.categories.some((category) => category.name === "NEW ARRIVALS");
+            return product.categories.some((category) => category.name === "LAST MINUTE");
         }
         return false;
     });
@@ -13,12 +15,24 @@ const HomeProducts = ({ products }) => {
     return (
         <>
             <div className="d-flex text-center justify-content-center align-items-center flex-direction-column">
-                <h1 className='bg-info'>Featured Products</h1>
-                {filteredProducts.map((product) => (
-                    <div key={product.id}>
-                        <p>{product.name}</p>
-                    </div>
-                ))}
+               
+            {Array.isArray(filteredProducts) && filteredProducts.map((card) => (
+                     <div className='card border-0' key={card.id}>
+                        <div className='card-img'>
+                           {card.images && card.images.length > 0 && (
+                              <img src={card.images[0].src} alt="products-card" style={{ width: "200px", height: "205px" }} />
+                           )}
+                           <div className="overlay">
+                              <EyeIcone card={card} />
+                              <AddToCart />
+                           </div>
+                        </div>
+
+                        <p className='text-center'>{`Dress ${card.name}`}</p>
+                        <p className='text-center' style={{ marginTop: "-15px" }} dangerouslySetInnerHTML={{ __html: card.description }} />
+                        <p className='text-center' style={{ marginTop: "-25px" }}>${card.price}.00</p>
+                     </div>
+                  ))}
             </div>
         </>
     );
